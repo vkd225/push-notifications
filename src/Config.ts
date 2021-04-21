821f5a4e23aaa87f4861e99891a0d8a1c949881e
vikashStr <vikash@strongarmtech.com>
2021-04-21 12:31:52 -0400

    :ghost: add env example

--- src/Config.ts
+++ src/Config.ts
@@ -1,8 +1,6 @@
 const Config = {
     content_type: "application/json; charset=utf-8",
     base_url: "https://onesignal.com",
-    // app_id: "b64f8d0d-5bb0-41f1-9fef-925c4af08c6b", // (localhost)
-    // app_id: "420bcf3c-c969-4d3e-b795-b1035de5a984",      // (Nick.org)
     app_id: "21448731-5b54-47ed-bb61-757d4cedc90b",    // (vikash.dev)
     included_segments: ["Subscribed Users"],
     api_key: process.env.REACT_APP_API_KEY
